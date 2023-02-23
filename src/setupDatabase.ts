22302f7adcb3336f83b9a9c32196e099a9f40c4f
Anvesh-affinsys <94594268+Anvesh-affinsys@users.noreply.github.com>
2023-02-24 00:18:11 +0530

    created setup project

---
+++ src/setupDatabase.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import Logger from 'bunyan';
+import { config } from './config';
+
+const log: Logger = config.createLogger('setupDatabase');
+
+export default () => {
+  const connect = () => {
+    mongoose
+      .connect('mongodb://localhost:27017/chattyapp-backend')
+      .then(() => {
+        log.info('Successfully connected to database.');
+      })
+      .catch((err) => {
+        log.error('Error connecting to database', err);
+        return process.exit(1);
+      });
+  };
+  connect();
+
+  mongoose.connection.on('disconnected', connect);
+};
